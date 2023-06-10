62c103072354a90cd38baf8b338f70c063880db3
yersoncp <yerson.rc@gmail.com>
2023-06-09 22:36:27 -0500

    update layouts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,25 +4,23 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex flex-col p-10">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+    <main className="p-6">
+      <div>
         {/* <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"> */}
-        <p className="left-0 top-0 flex w-full justify-center">
+        <p className="flex w-full justify-center py-6">
           Web components
         </p>
       </div>
 
-      <div className="my-4">
+      <div className="mb-6 grid2">
         <BlockRevenues />
-      </div>
-
-      <div className="my-4">
         <TrafficChannel />
       </div>
 
-      <div className="my-4">
+      <div className="mb-6 grid3">
         <BlockInsight />
       </div>
+
     </main>
   )
 }
